ca8555e83e411ce2429bf8209177e6c8ccf6f84b
大青虫 <46221967+daqingchong0809@users.noreply.github.com>
2020-09-10 19:21:54 +0800

    layout布局和导航

---
+++ src/components/layout/aside/index.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="aside">
+        <el-menu
+            default-active="1-4-1"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+            :collapse="isCollapse"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+        >
+            <el-submenu index="1">
+                <template slot="title">
+                    <i class="el-icon-menu"></i>
+                    <span slot="title">组件</span>
+                </template>
+                <el-menu-item-group>
+                    <el-menu-item index="1-1">选项1</el-menu-item>
+                    <el-menu-item index="1-2">选项2</el-menu-item>
+                </el-menu-item-group>
+
+                <el-submenu index="1-4">
+                    <span slot="title">选项4</span>
+                    <el-menu-item index="1-4-1">选项1</el-menu-item>
+                </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2">
+                <i class="el-icon-menu"></i>
+                <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+                <i class="el-icon-setting"></i>
+                <span slot="title">导航四</span>
+            </el-menu-item>
+        </el-menu>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isCollapse: false,
+        };
+    },
+    methods: {
+        handleOpen(key, keyPath) {
+            console.log(key, keyPath);
+        },
+        handleClose(key, keyPath) {
+            console.log(key, keyPath);
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.aside {
+    .el-menu-vertical-demo:not(.el-menu--collapse) {
+        width: 200px;
+        min-height: 400px;
+    }
+    .el-menu {
+        border-right: 0px;
+    }
+}
+</style>
---
+++ src/components/layout/index.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="layout">
+        <el-container>
+            <el-aside width="200px"><Aside></Aside></el-aside>
+            <el-container>
+                <el-header>Header</el-header>
+                <el-main>Main</el-main>
+            </el-container>
+        </el-container>
+    </div>
+</template>
+
+<script>
+import Aside from "./aside/index";
+export default {
+    components: {
+        Aside,
+    },
+    data() {
+        return {};
+    },
+    methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.layout {
+    height: 100vh;
+    background-color: rgb(84, 92, 100);
+    .el-aside {
+        height: 100vh;
+    }
+    .el-main {
+        background-color: #fff;
+    }
+}
+</style>
